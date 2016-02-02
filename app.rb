dfa7dcdf76538adfb48714064be78ef056597654
David Parry <drjparry@gmail.com>
2016-02-02 12:48:32 +0100

    added kitty cat

--- app.rb
+++ app.rb
@@ -1,9 +1,20 @@
 require 'sinatra'
 
+set :session_secret, 'super secret'
+
 get '/' do
-  "hello!"
+  "hello! hello"
 end
 
 get '/secret' do
   "ooo, you've accessed a secret message"
+end
+
+get '/cat' do
+ "<!DOCTYPE html>
+<html>
+ <div>
+ <img src='http://bit.ly/1eze8aE'>
+ </div>
+ </html>"
 end
